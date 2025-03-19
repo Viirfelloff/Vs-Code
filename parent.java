dfbb10999ff80df3630bff3001f991062dd18f30
Your Name <you@example.com>
2025-03-19 13:43:41 -0700

    changessss

---
+++ parent.java
@@ -0,0 +1,7 @@
+public abstract class parent {
+  public abstract void add(int x, int y);  
+  public abstract void subtrat (int x, int y);
+  public void manageInfo () {
+    System.out.println("Welcome to aululator!");
+  }
+}
--- sigma.java
+++ sigma.java
@@ -1,4 +1,4 @@
-public class sigma { 
+public class sigma  { 
 public static void main(String[] args) {
     System.out.println("sigma");
     }
