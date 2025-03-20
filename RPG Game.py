bdf72bf7510622eaa6c5b5b4164ac49b02cc44bc
Your Name <you@example.com>
2025-03-20 07:53:20 -0400

    lots of good python refreshing and progress

--- RPG Game.py
+++ RPG Game.py
@@ -1,5 +1,5 @@
 from idlelib.query import Query
-
+import random
 import IndivTypeTest
 from time import sleep
 def pregame():
@@ -12,8 +12,44 @@ def game():
     sleep(1)
     IndivTypeTest.IndivType("Now for some backstory:")
     print()
-    class character:
-        name = ""
+    def initChars():
+        chance = random.randint(0, 20)
+        class assassin:
+            def __init__(self, hlth, atk, defen, magic, crit):
+                self.hlth = hlth
+                self.atk = atk
+                self.defen = defen
+                self.magic = magic
+                self.crit = crit
+                def attack():
+                    self.crit = chance
+                    assDamageDealt = 0
+                    assDamageDealt += self.atk
+                    if (self.crit == 3):
+                        assDamageDealt += 5
+                    else:
+                        assDamageDealt += 0
+                def defense():
+                    assDamageTaken = 0;
+                    if (assDamageTaken > 0):
+                        assDamageTaken += -self.defen
+                        self.hlth += -assDamageTaken
+                    if (self.hlth == 0):
+                        print("You are Dead.....")
+
+        class mage:
+            def __init__(self, hlth, atk, defen, magic, crit):
+                self.hlth = hlth
+                self.atk = atk
+                self.defen = defen
+                self.magic = magic
+                self.crit = crit
+
+
+
+
+
+
 
 pregame()
 
