5f5697ac17bdcfa4dbf17951fb4ff1455d86431b
Your Name <you@example.com>
2025-03-21 08:44:49 -0400

    i hate java

--- parent.java
+++
@@ -1,7 +0,0 @@
-public abstract class parent {
-  public abstract void add(int x, int y);  
-  public abstract void subtrat (int x, int y);
-  public void manageInfo () {
-    System.out.println("Welcome to aululator!");
-  }
-}
--- sigma.java
+++
@@ -1,5 +0,0 @@
-public class sigma  { 
-public static void main(String[] args) {
-    System.out.println("sigma");
-    }
-}
