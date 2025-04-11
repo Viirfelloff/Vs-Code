80d4360e23173f3add0ec4bcfbfa8ccb3fd80327
Your Name <you@example.com>
2025-04-11 07:55:22 -0400

    suici

--- geometryDashNBASFAEGHAEWEGR.py
+++ geometryDashNBASFAEGHAEWEGR.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pygame
 from pygame import K_SPACE
 import keyboard
@@ -19,7 +21,7 @@ try:
     spike = pygame.transform.scale(spike, (50, 50))
     back = pygame.image.load("geo.jpg")
     player = pygame.image.load("gomeetry ahs.png")
-    player = pygame.transform.scale(player, (100, 100))
+    player = pygame.transform.scale(player, (75, 75))
     back = pygame.transform.scale(back, (length, width))
 except pygame.error as e:
     print(f"Error loading images: {e}")
@@ -46,7 +48,7 @@ playMusic("klutter.mp3")
 
 # Physics vars
 playerx = 100
-playery = 260  # Start on the ground
+playery = 283 # Start on the ground
 velocity_y = 0
 gravity = 1
 jump_strength = -17
@@ -57,20 +59,25 @@ spikecolx = spikex
 spikecoly = spikey
 global spikeDrawn
 spikeDrawn = False
+global control1
+control1 = False
+global control2
+control2 = False
 # Function to draw and move the spike
-def drawSpike():
-    spikeDrawn = False
-    global spikex  # Make spikex a global variable to modify it
-    spikecol = pygame.Rect(spikex, spikey, 20,30)
-    spikex -= 7
-    if spikex < -50:  # Reset the spike when it's off-screen
-        spikex = 900
-    if spikecol.colliderect(playerCol):
-        global running
-        running = False
-    display.blit(spike, (spikex, spikey))
+def drawSpike(x,y):
+    if spikeDrawn == False:
+        global spikex  # Make spikex a global variable to modify it
+        spikecol = pygame.Rect(x, y, 20,30)
+        x -= 7
+        if x < -50:  # Reset the spike when it's off-screen
+            x = 900
+        if spikecol.colliderect(playerCol):
+            global running
+            running = False
+        display.blit(spike, (x, y))
+    spike_positions = [
 
-
+    ]
 # Main game loop
 while running:
     clock.tick(60)
@@ -80,8 +87,8 @@ while running:
     playery += velocity_y
 
     # Ground collision
-    if playery >= 260:
-        playery = 260
+    if playery >= 283:
+        playery = 283
         velocity_y = 0
         on_ground = True
 
@@ -99,12 +106,6 @@ while running:
     # Draw everything
     display.blit(back, (0, 0))
     display.blit(player, (playerx, playery))
-    playerCol = pygame.Rect(playerx, playery, 100,100)
-    def moveSpike():
-        global spikeDrawn
-        if spikeDrawn == False:
-            drawSpike()
-            if spikex <= 0:
-                spikeDrawn = True
-    moveSpike()
+    playerCol = pygame.Rect(playerx, playery, 75,75)
+    drawSpike(spikex, spikey)
     pygame.display.update()
