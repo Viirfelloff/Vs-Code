33e79a13d1a5264ca8beb7e85616a1f996271d09
Your Name <you@example.com>
2025-04-21 07:27:54 -0400

    sucid

---
+++ TwoPlayerTTT.py
@@ -0,0 +1,13 @@
+from time import sleep
+print("Welcome to 2 player Tic-Tac-Toe.")
+sleep(1)
+input1 = input("Press Enter to begin.")
+
+
+def drawBoard():
+    print("-----------")
+
+
+if input1 == "":
+    print("Game has started!")
+    drawBoard()
