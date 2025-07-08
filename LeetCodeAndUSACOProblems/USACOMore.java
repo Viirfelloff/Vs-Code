a1500bebe35f283842c3982de622f110db8c348f
viir <vsood28@scarsdaleschools.org>
2025-07-08 10:56:09 -0400

    les go

---
+++ LeetCodeAndUSACOProblems/USACOMore.java
@@ -0,0 +1,27 @@
+package LeetCodeAndUSACOProblems;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+//yo soy el
+public class USACOMore {
+    public static int[] solution (int cows, int[] order, int[] mixed) {
+        int[] ordered = new int[cows];
+        Map<Integer, Integer> non = new HashMap<>();
+        for (int i = 0; i < order.length; i++) {
+            non.put(order[i], mixed[i]);
+        }
+        for (int i = 0; i < order.length; i++) {
+            ordered[i] = non.get(i + 1);
+        }
+        return ordered;
+    }
+    public static void main (String[] args) {
+        int[] order = {1,3,4,5,2};
+        int[] mixed = {1234567,2222222,3333333,4444444,5555555};
+        int[] res = solution(5,order, mixed);
+        for (int s : res) {
+            System.out.println(s);
+        }
+    }
+}
