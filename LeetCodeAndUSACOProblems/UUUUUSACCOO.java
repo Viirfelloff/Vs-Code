bcc8c4a79d9e3ad77a82d44f5e34ae49828189a7
viir <vsood28@scarsdaleschools.org>
2025-07-09 17:59:50 -0400

    too easy

---
+++ LeetCodeAndUSACOProblems/UUUUUSACCOO.java
@@ -0,0 +1,9 @@
+package LeetCodeAndUSACOProblems;
+
+public class UUUUUSACCOO {
+    public static int solution (int num, int[] turns) {
+        return 0;
+    }
+    public static void main(String[] args) {
+    }
+}
