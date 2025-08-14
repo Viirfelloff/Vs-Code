2d2cafad57218a6c4e04844b7f544d1325fcc107
viir <vsood28@scarsdaleschools.org>
2025-08-14 19:52:32 -0400

    sewi

---
+++ LeetCodeAndUSACOProblems/CodeForces1175A.java
@@ -0,0 +1,21 @@
+package LeetCodeAndUSACOProblems;
+
+import java.util.Scanner;
+
+public class CodeForces1175A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int ops = 0;
+        while (n != 0) {
+            if (n % k == 0) {
+                n /= k;
+            } else {
+                n -= 1;
+            }
+            ops++;
+        }
+        System.out.println(ops);
+    }
+}
--- LeetCodeAndUSACOProblems/USACOLying.java
+++ LeetCodeAndUSACOProblems/USACOLying.java
@@ -1,10 +1,27 @@
 package LeetCodeAndUSACOProblems;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
-//revisit when ready
 public class USACOLying {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int lower = 0;
+        int upper = 0;
+        Map<String, Integer> map1 = new LinkedHashMap<>();
+        for (int i = 0; i < n; i++) {
+            String dir = sc.next();
+            int range = sc.nextInt();
+            if (i == 0) {
+                if (dir.equals('G')) {
+                    lower = range;
+                } else {
+                    upper = range;
+                }
+            } else {
 
+            }
+        }
     }
 }
