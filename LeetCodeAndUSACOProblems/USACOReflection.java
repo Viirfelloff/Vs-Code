35601334df98e5dd2276e8e0912fdc3e3507cc51
Your Name <you@example.com>
2025-09-14 18:30:41 -0400

    frigma

--- LeetCodeAndUSACOProblems/USACOReflection.java
+++ LeetCodeAndUSACOProblems/USACOReflection.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-// we are NOT getting into silver
+//def new hardest gah dayum
 public class USACOReflection {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,33 +18,63 @@ public class USACOReflection {
         for (int j = 0; j < u; j++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            updates.add(Arrays.asList(x,y));
+            updates.add(Arrays.asList(x, y));
         }
-        for (int k = 0; k < updates.size(); k++) {
-            int r = updates.get(k).get(0);
-            int c = updates.get(k).get(1);
-            if (rows.get(r)[c] == '#') {
-                rows.get(r)[c] = '.';
-            } else {
-                rows.get(r)[c] = '#';
-            }
 
+        int flips = computeFlips(rows, n);
+        List<Integer> res = new ArrayList<>();
+        res.add(flips);
+
+        for (List<Integer> upd : updates) {
+            int r = upd.get(0) - 1;
+            int c = upd.get(1) - 1;
+
+            // map to canonical representative in top-left quadrant
+            int r0 = Math.min(r, n - 1 - r);
+            int c0 = Math.min(c, n - 1 - c);
+
+            // subtract old contribution
+            flips -= countdiffs(rows, r0, c0, n);
+
+            // flip the cell
+            rows.get(r)[c] = (rows.get(r)[c] == '.') ? '#' : '.';
+
+            // add new contribution
+            flips += countdiffs(rows, r0, c0, n);
+
+            res.add(flips);
+        }
+
+        for (int val : res) {
+            System.out.println(val);
         }
     }
-    public void countdiffs (List<char[]> painting, int n) {
-        for (int l = 0; l < painting.size(); l++) {
-            for (int h = 0; h < painting.get(l).length; h++) {
-                int r1 = l;
-                int c1 = h;
-                int r2 = r1;
-                int c2 = n - 1 - c1;
-                int r3 = n - 1 - r1;
-                int c3 = c1;
-                int r4 = n - 1 - r1;
-                int c4 = n - 1 - c1;
-                int diffs = 0;
 
+    public static int countdiffs(List<char[]> painting, int r, int c, int n) {
+        int r1 = r;
+        int c1 = c;
+        int r2 = r1;
+        int c2 = n - 1 - c1;
+        int r3 = n - 1 - r1;
+        int c3 = c1;
+        int r4 = n - 1 - r1;
+        int c4 = n - 1 - c1;
+        int zerocount = 0;
+        int onecount = 0;
+        if (painting.get(r1)[c1] == '.') zerocount++; else onecount++;
+        if (painting.get(r2)[c2] == '.') zerocount++; else onecount++;
+        if (painting.get(r3)[c3] == '.') zerocount++; else onecount++;
+        if (painting.get(r4)[c4] == '.') zerocount++; else onecount++;
+        return Math.min(onecount, zerocount);
+    }
+
+    public static int computeFlips(List<char[]> pnt, int n) {
+        int diff = 0;
+        for (int m = 0; m < n / 2; m++) {
+            for (int v = 0; v < n / 2; v++) {
+                diff += countdiffs(pnt, m, v, n);
             }
         }
+        return diff;
     }
 }
