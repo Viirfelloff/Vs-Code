2ebdc79bbf3d063b6d33db4dfb6e86ef295f3248
Your Name <you@example.com>
2025-10-30 21:02:17 -0400

    LETS GO WE DID IT FINALLY MAKIGN PROGRESS

--- LeetCodeAndUSACOProblems/CodeForces2137D.java
+++ LeetCodeAndUSACOProblems/CodeForces2137D.java
@@ -1,5 +1,6 @@
+import javax.swing.plaf.IconUIResource;
 import java.util.*;
-
+//LETS GOOOO WE DID IT OUR FIRST 1200 WITH ZERO HELP
 public class CodeForces2137D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,15 +11,37 @@ public class CodeForces2137D {
             for (int j = 0; j < n; j++) {
                 occ.add(sc.nextInt());
             }
-            //implementation: make a new element for each thing going over the limit
-            //ex 2 1s = 2 elements occur once. 4 2s. two elements occur twice.
-            //then using the order output an array following the constraints.
-            Map<Integer, Integer> occocc = new HashMap<>();
-            for (int x : occ) {
-                occocc.put(x, occocc.getOrDefault(x, 0) + 1);
+            Map<Integer, Integer> occoccpre = new HashMap<>();
+            int totalElements = 0;
+            for (int x: occ) {
+                occoccpre.put(x, occoccpre.getOrDefault(x, 0) + 1);
+                if (occoccpre.get(x) == x) {
+                    totalElements += x;
+                    occoccpre.put(x,0);
+                }
             }
-            boolean valid = true;
-
+            if (totalElements != n) {
+                System.out.println(-1);
+            } else {
+                Map<Integer, Integer> occocc = new HashMap<>();
+                List<Integer> res = new ArrayList<>();
+                int curElement = n + 1;
+                Map<Integer, Integer> elementToSolution = new HashMap<>();
+                for (int x : occ) {
+                    occocc.put(x, occocc.getOrDefault(x, 0) + 1);
+                    if (!elementToSolution.containsKey(x)) {
+                        curElement--;
+                        elementToSolution.put(x, curElement);
+                    }
+                    else if (occocc.get(x) > x) {
+                        curElement--;
+                        elementToSolution.put(x, curElement);
+                        occocc.put(x, 1);
+                    }
+                    res.add(elementToSolution.get(x));
+                }
+                for (int x : res) System.out.println(x);
+            }
         }
     }
 }
