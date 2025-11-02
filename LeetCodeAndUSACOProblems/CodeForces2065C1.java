9155fa4f9abd416654b4965af6abb1da7fa99832
Your Name <you@example.com>
2025-11-01 21:59:58 -0400

    sigmasgiasgweg

--- LeetCodeAndUSACOProblems/CodeForces2065C1.java
+++ LeetCodeAndUSACOProblems/CodeForces2065C1.java
@@ -1,18 +1,34 @@
-package LeetCodeAndUSACOProblems;
-
 import java.util.*;
 
-
-//scan decreasing pairs, apply op on each, take increasing sequence from that
-//if at any point a decreasing pair cannot be fixed or inevitably messes up another pair, break and return false
 public class CodeForces2065C1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        for (int i = 0; i< t; i++) {
-            List<Integer> list  = new ArrayList<>();
-            for (int j = 0; j < sc.nextInt(); j++) list.add(sc.nextInt());
-            
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            int X = sc.nextInt();
+
+            long prev = Long.MIN_VALUE;
+            boolean ok = true;
+
+            for (int i = 0; i < n; i++) {
+                long lo = Math.min(a[i], X - a[i]);
+                long hi = Math.max(a[i], X - a[i]);
+
+                if (lo >= prev) {
+                    prev = lo;
+                } else if (hi >= prev) {
+                    prev = hi;
+                } else {
+                    ok = false;
+                    break;
+                }
+            }
+
+            System.out.println(ok ? "YES" : "NO");
         }
     }
 }
