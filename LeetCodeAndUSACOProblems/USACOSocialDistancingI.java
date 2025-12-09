609faafb734e9eebd474c2fd6277ba85ccea5bad
Your Name <you@example.com>
2025-12-09 18:37:33 -0500

    half solved

--- LeetCodeAndUSACOProblems/USACOSocialDistancingI.java
+++ LeetCodeAndUSACOProblems/USACOSocialDistancingI.java
@@ -1,7 +1,5 @@
-package LeetCodeAndUSACOProblems;
-
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class USACOSocialDistancingI {
     public static void main(String[] args) throws IOException {
@@ -9,12 +7,87 @@ public class USACOSocialDistancingI {
         PrintWriter pr = new PrintWriter(new FileWriter("socdist1.out"));
         int N = Integer.parseInt(br.readLine());
         String s = String.valueOf(br.readLine());
-        //essentially, maximize D.
-        //to do this, take the current D. D * 2 + 1 is the minimum length of 0 block for preserving D.
-        //if none exist, decrease D and repeat.
-        //after, its the second cow. repeat the same process.
-        br.close();
+        int oneCount = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') oneCount++;
+        }
+        if (oneCount == 0) System.out.println(N);
+
+        for (int j = 0; j < 2; j++) {
+            List<int[]> blocks = blocks(s);
+            int max = -1;
+            int[] maxArr = new int[]{-1};
+            for (int[] x : blocks) {
+                if (x[1] - x[0] + 1 > max) {
+                    max = x[1] - x[0] + 1;
+                    maxArr = x;
+                }
+            }
+            int maxD = max;
+            int dist = getDist(maxD);
+            int minD = Integer.MAX_VALUE;
+            int lastOne = -1;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '1') {
+                    if (lastOne != -1) {
+                        int d = i - lastOne;
+                        if (d < minD) minD = d;
+                    }
+                    lastOne = i;
+                }
+            }
+            if (dist >= minD) {
+                //replace with minD
+                StringBuilder sb = new StringBuilder();
+                sb.append(s);
+                sb.replace(maxArr[0] + minD, maxArr[0] + minD + 1,"1");
+                s = sb.toString();
+            } else {
+                //replace with dist
+                StringBuilder sb = new StringBuilder();
+                sb.append(s);
+                sb.replace(maxArr[0] + dist, maxArr[0] + dist + 1, "1");
+                s = sb.toString();
+            }
+        }
+
+        //now, s = final string. compute min D.
+        int minD = Integer.MAX_VALUE;
+        int lastOne = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                if (lastOne != -1) {
+                    int d = i - lastOne;
+                    if (d < minD) minD = d;
+                }
+                lastOne = i;
+            }
+        }
+
+        pr.println(minD);
         pr.flush();
         pr.close();
+    }
+
+    public static List<int[]> blocks(String s) {
+        List<int[]> blocks = new ArrayList<>();
+        int st = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0' && st == -1) {
+                st = i; // start of a zero-block
+            }
+            if (s.charAt(i) == '1' && st != -1) {
+                blocks.add(new int[]{st, i - 1}); // end of zero-block
+                st = -1;
+            }
+        }
+        if (st != -1) {
+            blocks.add(new int[]{st, s.length() - 1}); // add last block if string ends with 0
+        }
+        return blocks;
+    }
+
+    public static int getDist(int size) {
+        return (size - 1) / 2;
     }
 }
