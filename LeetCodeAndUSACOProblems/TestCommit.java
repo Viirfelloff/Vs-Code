bc410d6a3a8623a34eea647dd128e09f2cc7d07e
Viir Sood <vsood28@scarsdaleschools.org>
2025-12-24 13:05:32 -0500

    i hate this

---
+++ LeetCodeAndUSACOProblems/TestCommit.java
@@ -0,0 +1,10 @@
+package LeetCodeAndUSACOProblems;
+
+import java.util.Scanner;
+
+public class TestCommit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+    }
+}
