aceebbc7ce03bc136b364e9a38a9fa45e3f5d55c
Viirfelloff <vsood28@scarsdaleschools.org>
2026-02-07 15:17:32 -0500

    LES GOOOOOOOOOO

---
+++ LeetCodeAndUSACOProblems/CodeForces1868A.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+public class CodeForces1868A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[][] grid = new int[n][m];
+            if (m == 1) {
+                System.out.println(0);
+                for (int j = 0; j < n; j++) System.out.println(0);
+            } else {
+                if (n >= m) {
+                    System.out.println(m);
+                    grid[0][0] = (m - 1);
+                    for (int j = 0; j < m - 1; j++) grid[0][j + 1] = j;
+                    int[] standard = grid[0].clone();
+                    if (m >= 2) {
+                        //decrease filling
+                        int upperLim = 1; //number u need to start at inclusive
+                        //top row should have upperLim
+                        for (int col = 2; col < m; col++) {
+                            int current = upperLim;
+                            for (int j = 0; j < upperLim + 1; j++) {
+                                grid[j][col] = current;
+                                current--;
+                            }
+                            upperLim++;
+                        }
+                    }
+                    int offset = 1;
+                    for (int row = 1; row < n; row++) {
+                        if (offset >= m - 1) grid[row] = standard;
+                        else {
+                            //fixed pattern logic
+                            int pointer = 0;
+                            for (int j = m - 1 - offset; j < m; j++) {
+                                grid[row][pointer] = j;
+                                pointer++;
+                            }
+                        }
+                        offset++;
+                    }
+                    printGrid(grid);
+                } else {
+                    System.out.println(n + 1);
+                    grid[0][0] = (m - 1);
+                    for (int j = 0; j < m - 1; j++) grid[0][j + 1] = j;
+                    int[] standard = grid[0].clone();
+                    if (m >= 2) {
+                        //decrease filling
+                        int upperLim = 1; //number u need to start at inclusive
+                        //top row should have upperLim
+                        for (int col = 2; col < m; col++) {
+                            int current = upperLim;
+                            for (int j = 0; j < upperLim + 1  && j < n; j++) {
+                                grid[j][col] = current;
+                                current--;
+                            }
+                            upperLim++;
+                        }
+                    }
+                    int offset = 1;
+                    for (int row = 1; row < n; row++) {
+                        if (offset >= m - 1) grid[row] = standard;
+                        else {
+                            //fixed pattern logic
+                            int pointer = 0;
+                            for (int j = m - 1 - offset; j < m; j++) {
+                                grid[row][pointer] = j;
+                                pointer++;
+                            }
+                        }
+                        offset++;
+                    }
+                    printGrid(grid);
+                }
+            }
+        }
+    }
+    public static void printGrid(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println(); // new line after each row
+        }
+    }
+}
---
+++ LeetCodeAndUSACOProblems/CodeForces2187A.java
@@ -0,0 +1,50 @@
+package LeetCodeAndUSACOProblems;
+
+import java.util.*;
+
+public class CodeForces2187A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            List<Long> list = new ArrayList<>();
+            Map<Long, List<Integer>> occ = new HashMap<>();
+            for (int j = 0; j < n; j++) {
+                long x = sc.nextLong();
+                if (occ.containsKey(x)) {
+                    List<Integer> listt = occ.get(x);
+                    listt.add(j);
+                    occ.put(x,listt);
+                } else {
+                    List<Integer> listt = new ArrayList<>();
+                    listt.add(j);
+                    occ.put(x,listt);
+                }
+                list.add(x);
+            }
+            //idea: this is really scuffed but take all the numbers and search in ascenidng order
+            //then, for each number, see the number in the sequence it is farthest away from.
+            //using min and max for the above. if it is already in the desired spot,
+            //add to list and swap. then change array. if not, we need to swap something else for it
+            //so that number in the spot rn and the desired number swap + into place swap have to maximize k.
+            //to do this, if they have the same farthest away number, leave it. if not, do some compromise idk.
+            //then perform both swaps. keep a list of in palce and not in palce.
+            //if u deisre a number and it is in place, u need to perform 3 swaps to put it back afterward.
+            //this should be the best strategy
+            Set<Long> inPlace = new HashSet<>();
+            long min = Collections.min(list);
+            long max = Collections.max(list);
+            List<Long> sorted = new ArrayList<>(list);
+            Collections.sort(sorted);
+            for (long x : sorted) {
+                long farthest = Math.max(Math.abs(x - min), Math.abs(x - max)) == Math.abs(x - max) ? max : min;
+                if (inPlace.contains(x)) {
+
+                } else {
+                    inPlace.add(x);
+                }
+            }
+        }
+    }
+}
