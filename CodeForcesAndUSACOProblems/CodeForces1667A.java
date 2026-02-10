9d30866c86b293e07fcce1ba25c8337595b05a43
Viirfelloff <vsood28@scarsdaleschools.org>
2026-02-09 21:09:24 -0500

    1300s really getting too easy bro

--- CodeForcesAndUSACOProblems/CodeForces1667A.java
+++ CodeForcesAndUSACOProblems/CodeForces1667A.java
@@ -1,4 +1,92 @@
-package CodeForcesAndUSACOProblems;
+import java.util.*;
 
 public class CodeForces1667A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Long> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) list.add(sc.nextLong());
+        List<Long> moves = new ArrayList<>();
+        //we consider every index as the 'zero' point.
+        //propagate left and right, making increasing negative and positive arrays,
+        //respectively. add moves to list and take minimum
+        for (int i = 0; i < n; i++) {
+            long[] arr = new long[n];
+            //for right - left, if the current number > the next to the right, we can ignore.
+            //else, the amount you need to decrease is the floor of right / current + 1.
+            //or use int division
+            //add this number to total moves.
+            //for left - right, if current num < next to left, ignore
+            //else use the same formula, moves += floor of (left / cur).
+            // ex: 7, 2. moves += floor(3.5) + 1, which is 4.
+            long dec = 0;
+            if (i == 0) {
+                for (int j = i + 1; j < n; j++) {
+                    if (j == i + 1) {
+                        arr[j] = list.get(j);
+                        dec++;
+                    } else {
+                        if (Math.abs(arr[j - 1]) >= list.get(j)) {
+                            long times = (arr[j - 1] / list.get(j)) + 1;
+                            dec += times;
+                            arr[j] = (list.get(j) * times);
+                        } else {
+                            dec++;
+                            arr[j] = list.get(j);
+                        }
+                    }
+                }
+            }
+            else if (i == n - 1) {
+                for (int j = i - 1; j >= 0; j--) {
+                    if (j == i - 1) {
+                        arr[j] = -list.get(j);
+                        dec++;
+                    } else {
+                        if (list.get(j) <= Math.abs(arr[j + 1])) {
+                            long times = (-arr[j + 1] / list.get(j)) + 1;
+                            dec += times;
+                            arr[j] = -(list.get(j) * times);
+                        } else {
+                            dec++;
+                            arr[j] = -list.get(j);
+                        }
+                    }
+                }
+            } else {
+                for (int j = i - 1; j >= 0; j--) {
+                    if (j == i - 1) {
+                        arr[j] = -list.get(j);
+                        dec++;
+                    } else {
+                        if (list.get(j) <= Math.abs(arr[j + 1])) {
+                            long times = (-arr[j + 1] / list.get(j)) + 1;
+                            dec += times;
+                            arr[j] = -(list.get(j) * times);
+                        } else {
+                            dec++;
+                            arr[j] = -list.get(j);
+                        }
+                    }
+                }
+                for (int j = i + 1; j < n; j++) {
+                    if (j == i + 1) {
+                        arr[j] = list.get(j);
+                        dec++;
+                    } else {
+                        if (Math.abs(arr[j - 1]) >= list.get(j)) {
+                            long times = (arr[j - 1] / list.get(j)) + 1;
+                            dec += times;
+                            arr[j] = (list.get(j) * times);
+                        } else {
+                            dec++;
+                            arr[j] = list.get(j);
+                        }
+                    }
+                }
+            }
+            moves.add(dec);
+        }
+        System.out.println(Collections.min(moves));
+    }
 }
--- CodeForcesAndUSACOProblems/USACOEvenMoreOddPhotos.java
+++ CodeForcesAndUSACOProblems/USACOEvenMoreOddPhotos.java
@@ -1,63 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class USACOEvenMoreOddPhotos {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) list.add(sc.nextInt());
-        //remember - group to alternate between even and odd.
-        //extra evens can be ignored because they do not change parity.
-        //even groups can be achieved through an even number of odds or one even.
-        //odd groups can be achieved through odd numbers of odds.
-        //if you have any number of extra evens, ignore and go w current groups.
-        //to get the extra odds, alternate, constructing groups of evens and odds
-        //for an even group immediately look for one even. odds are 1 odd.
-        //if you run out of odds, end and print the groups
-        //if you run out of evens, the extra odds will change the parity of anything you combine them with.
-        //take each group of odds, and add them together to obtain evens.
-        //adding these will make no difference.
-        //if you have an odd number of extra odds, this operation will leave one odd remaining.
-        //to resolve this, take 2 cases. you have ran out of evens, so the last group you formed was odd.
-        //you have to then, have an equal amount of odd and even groups. so you cannot change the parity of any one group.
-        //we cannot add or change groups, so we have to subtract groups
-        //let's try subtracting one group. it can be shown that this is the most optimal.
-        //combine an even and an odd group from the beginning to maintain order.
-        //even + odd = odd. we then add this extra to our new group. it becomes even.
-        //the last group we formed was odd, so we can append this new even and be done!!!!!
-        int evens = 0;
-        int odds = 0;
-        for (int x : list) {
-            if (x % 2 == 0) evens++;
-            else odds++;
+        int O = 0, E = 0;
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            if (x % 2 == 0) E++;
+            else O++;
         }
-        int groups = 0;
+        int res = 0;
         int parity = 0;
-        while (evens != 0 || odds != 0) {
-            if (evens == 0) {
-                groups++;
-                if (odds % 2 == 0) break;
-                else {
-                    groups--;
+        while (O > 0 || E > 0) {
+            if (parity % 2 == 0) {
+                if (E > 0) {
+                    res++;
+                    E--;
+                } else if (O >= 2) {
+                    res++;
+                    O -= 2;
+                } else if (O == 1) {
+                    res--;
                     break;
                 }
-            }
-            if (odds == 0) {
-                groups++;
-                break;
-            }
-            if (evens != 0 && odds != 0) {
-                if (parity % 2 == 0) {
-                    evens--;
-                } else {
-                    odds--;
-                }
-                groups++;
+            } else {
+                if (O > 0) {
+                    res++;
+                    O--;
+                } else break;
             }
             parity++;
         }
-        System.out.println(groups);
+        System.out.println(res);
     }
 }
