a44d9e5e4f085059582957ae19b24fd308b91ec3
Viir Sood <vsood28@scarsdaleschools.org>
2026-02-19 20:38:08 -0500

    frigma

--- CodeForcesAndUSACOProblems/USACOYearOfTheCow.java
+++ CodeForcesAndUSACOProblems/USACOYearOfTheCow.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class USACOYearOfTheCow {
     public static void main(String[] args) {
+        //golly bro finally ts stupid ass problem
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
         List<String[]> instr = new ArrayList<>();
@@ -9,14 +10,32 @@ public class USACOYearOfTheCow {
                 "Goat", "Monkey", "Rooster", "Dog", "Pig", "Rat"};
         Map<String, Integer> yeartoidx = new HashMap<>();
         for (int i = 0; i < 12; i++) yeartoidx.put(zodiac[i], i);
-        String targetEnd = "Bessie";
+        List<String> chain = new ArrayList<>();
+        chain.add("Elsie");
+        while (true) {
+            for (int i = 0; i < n; i++) {
+                if (instr.get(i)[0].equals(chain.get(chain.size() - 1))) {
+                    chain.add(instr.get(i)[7]);
+                    break;
+                }
+            }
+            if (chain.get(chain.size() - 1).equals("Bessie")) break;
+        }
+        int pointer = chain.size() - 1;
+        int pointer2 = chain.size() - 2;
         int cumulative = 0;
         String currentyear = "Ox";
         while (true) {
+            String targetEnd = chain.get(pointer);
+            if (targetEnd.equals("Elsie")) {
+                System.out.println(Math.abs(cumulative));
+                break;
+            }
+            String targetStart = chain.get(pointer2);
             String[] target = new String[8];
             for (int i = 0; i < instr.size(); i++) {
                 String[] in = instr.get(i);
-                if (in[7].equals(targetEnd)) {
+                if (in[7].equals(targetEnd) && in[0].equals(targetStart)) {
                     target = in;
                     break;
                 }
@@ -34,11 +53,8 @@ public class USACOYearOfTheCow {
             }
             cumulative += dist;
             currentyear = target[4];
-            targetEnd = target[0];
-            if (targetEnd.equals("Elsie")) {
-                System.out.println(Math.abs(cumulative));
-                break;
-            }
+            pointer--;
+            pointer2--;
         }
     }
 }
